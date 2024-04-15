e87cc20b55b6daf13114ff2ad5c87fdd17a24d9f
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-04-15 16:24:37 +0530

    Update autopic.py

--- plugins/autopic.py
+++ plugins/autopic.py
@@ -1,84 +1,97 @@
-# Ultroid - UserBot
-# Copyright (C) 2021-2023 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
-
-import asyncio
-import os
-import random
-from random import shuffle
+from pathlib import Path
+from random import choice
 
 from telethon.tl.functions.photos import UploadProfilePhotoRequest
 
-from pyUltroid.fns.helper import download_file
-from pyUltroid.fns.tools import get_google_images
+from pyUltroid.custom.bing_image import BingScrapper
 
-from . import LOGS, get_help, get_string, udB, ultroid_bot, ultroid_cmd
+from . import (
+    LOGS,
+    osremove,
+    get_help,
+    get_string,
+    scheduler,
+    udB,
+    ultroid_bot,
+    ultroid_cmd,
+)
+
 
 __doc__ = get_help("help_autopic")
 
 
+async def get_or_fetch_image(autopic_dir):
+    diriter = lambda p: list(p.iterdir())
+    autopic_dir = Path(autopic_dir)
+    if not (autopic_dir.is_dir() and (pics := diriter(autopic_dir))):
+        query = autopic_dir.name.removeprefix("bing-")
+        bing = BingScrapper(query=query, limit=120, filter="photo")
+        autopic_dir = await bing.download()
+        udB.set_key("AUTOPIC", autopic_dir)
+        pics = diriter(Path(autopic_dir))
+    return str(choice(pics))
+
+
+async def autopic_func():
+    if not (autopic_dir := udB.get_key("AUTOPIC")):
+        return
+    try:
+        path = await get_or_fetch_image(autopic_dir)
+    except Exception as exc:
+        return LOGS.exception(f"Autopic Error: {exc}")
+    try:
+        tg_file = await ultroid_bot.upload_file(path)
+        await ultroid_bot(UploadProfilePhotoRequest(file=tg_file))
+        osremove(path)
+    except Exception as exc:
+        LOGS.warning(f"autopic error in {path}: {exc}")
+
+
 @ultroid_cmd(pattern="autopic( (.*)|$)")
 async def autopic(e):
-    search = e.pattern_match.group(1).strip()
-    if udB.get_key("AUTOPIC") and not search:
+    search = e.pattern_match.group(2)
+    autopic_dir = udB.get_key("AUTOPIC")
+    if autopic_dir and (not search or search == "stop"):
         udB.del_key("AUTOPIC")
+        osremove(autopic_dir, folders=True)
+        if scheduler:
+            scheduler.remove_job("autopic")
         return await e.eor(get_string("autopic_5"))
+
     if not search:
-        return await e.eor(get_string("autopic_1"), time=5)
-    e = await e.eor(get_string("com_1"))
-    gi = googleimagesdownload()
-    args = {
-        "keywords": search,
-        "limit": 50,
-        "format": "jpg",
-        "output_directory": "./resources/downloads/",
-    }
+        return await e.eor(get_string("autopic_1"), time=6)
+
+    if not scheduler:
+        return await e.eor("`APScheduler is missing, Can't Use Autopic`", time=6)
+
+    eris = await e.eor(get_string("com_1"))
     try:
-        pth = await gi.download(args)
-        ok = pth[0][search]
-    except Exception as er:
-        LOGS.exception(er)
-        return await e.eor(str(er))
-    if not ok:
-        return await e.eor(get_string("autopic_2").format(search), time=5)
-    await e.eor(get_string("autopic_3").format(search))
-    udB.set_key("AUTOPIC", search)
-    SLEEP_TIME = udB.get_key("SLEEP_TIME") or 1221
-    while True:
-        for lie in ok:
-            if udB.get_key("AUTOPIC") != search:
-                return
-            file = await e.client.upload_file(lie)
-            await e.client(UploadProfilePhotoRequest(file))
-            await asyncio.sleep(SLEEP_TIME)
-        shuffle(ok)
+        path = await get_or_fetch_image(f"resources/downloads/bing-{search}")
+    except Exception as exc:
+        LOGS.exception(f"Autopic Error: {exc}")
+        return await eris.eor(get_string("autopic_2").format(search), time=10)
 
-
-if search := udB.get_key("AUTOPIC"):
-    images = {}
+    udB.set_key("AUTOPIC", str(Path(path).parent))
+    await eris.edit(get_string("autopic_3").format(search))
     sleep = udB.get_key("SLEEP_TIME") or 1221
+    scheduler.add_job(
+        autopic_func,
+        trigger="interval",
+        seconds=sleep,
+        id="autopic",
+        jitter=60,
+    )
 
-    async def autopic_func():
-        search = udB.get_key("AUTOPIC")
-        if images.get(search) is None:
-            images[search] = await get_google_images(search)
-        if not images.get(search):
-            return
-        img = random.choice(images[search])
-        filee = await download_file(img["original"], "resources/downloads/autopic.jpg")
-        file = await ultroid_bot.upload_file(filee)
-        await ultroid_bot(UploadProfilePhotoRequest(file))
-        os.remove(filee)
 
-    try:
-        from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-        schedule = AsyncIOScheduler()
-        schedule.add_job(autopic_func, "interval", seconds=sleep)
-        schedule.start()
-    except ModuleNotFoundError as er:
-        LOGS.error(f"autopic: '{er.name}' not installed.")
+if udB.get_key("AUTOPIC"):
+    sleep = udB.get_key("SLEEP_TIME") or 1221
+    if scheduler:
+        scheduler.add_job(
+            autopic_func,
+            "interval",
+            seconds=sleep,
+            id="autopic",
+            jitter=60,
+        )
+    else:
+        LOGS.error(f"autopic: 'apscheduler' is not installed.")
