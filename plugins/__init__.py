7638fba758192a24b8c0302aacb5571b711946c6
AnnayanX <148120781+AnnayanX@users.noreply.github.com>
2024-04-15 16:42:47 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,5 @@
 # Ultroid - UserBot
-# Copyright (C) 2021-2022 TeamUltroid
+# Copyright (C) 2021-2023 TeamUltroid
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
@@ -7,7 +7,6 @@
 
 import asyncio
 import os
-import re
 import time
 from random import choice
 
@@ -23,36 +22,22 @@ from pyUltroid.dB import DEVLIST, ULTROID_IMAGES
 from pyUltroid.fns.helper import *
 from pyUltroid.fns.misc import *
 from pyUltroid.fns.tools import *
-
-# from pyUltroid.startup._database import _BaseDatabase
+from pyUltroid.startup._database import _BaseDatabase as Database
 from pyUltroid.version import __version__, ultroid_version
 from strings import get_help, get_string
 
-# custom
-from pyUltroid.custom.commons import *
-from pyUltroid.custom.functions import *
-from pyUltroid.custom.mediainfo import gen_mediainfo
-
-
-quotly = Quotly()
-Telegraph = telegraph_client()
+udB: Database
 
 Redis = udB.get_key
 con = TgConverter
-
-# udB: _BaseDatabase
-# asst: UltroidClient
-# ultroid_bot: UltroidClient
-
+quotly = Quotly()
 OWNER_NAME = ultroid_bot.full_name
 OWNER_ID = ultroid_bot.uid
 
-LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
-TAG_LOG = udB.get_key("TAG_LOG")
+ultroid_bot: UltroidClient
+asst: UltroidClient
 
-# used in plugins, don't touch
-InlinePlugin = {}
-STUFF = {}
+LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
 
 
 def inline_pic():
@@ -64,14 +49,19 @@ def inline_pic():
     return INLINE_PIC
 
 
-# extra stuff
+Telegraph = telegraph_client()
+
 List = []
 Dict = {}
+InlinePlugin = {}
 N = 0
+cmd = ultroid_cmd
+STUFF = {}
 
 # Chats, which needs to be ignore for some cases
 # Considerably, there can be many
 # Feel Free to Add Any other...
+
 NOSPAM_CHAT = [
     -1001361294038,  # UltroidSupportChat
     -1001387666944,  # PyrogramChat
@@ -81,7 +71,20 @@ NOSPAM_CHAT = [
     -1001473548283,  # SharingUserbot
 ]
 
-ATRA_COL = (
+KANGING_STR = [
+    "Using Witchery to kang this sticker...",
+    "Plagiarising hehe...",
+    "Inviting this sticker over to my pack...",
+    "Kanging this sticker...",
+    "Hey that's a nice sticker!\nMind if I kang?!..",
+    "Hehe me stel ur stiker...",
+    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
+    "Roses are red violets are blue, kanging this sticker so my pack looks cool",
+    "Imprisoning this sticker...",
+    "Mr.Steal-Your-Sticker is stealing this sticker... ",
+]
+
+ATRA_COL = [
     "DarkCyan",
     "DeepSkyBlue",
     "DarkTurquoise",
@@ -94,13 +97,4 @@ ATRA_COL = (
     "Azure",
     "Moccasin",
     "PowderBlue",
-)
-
-
-async def _get_colors(pick=True):
-    path = "resources/colorlist.txt"
-    if os.path.exists(path):
-        data = await asyncread(path)
-        data = data.splitlines()
-        return choice(data) if pick else data
-    return "RoyalBlue" if pick else ["RoyalBlue"]
+]
